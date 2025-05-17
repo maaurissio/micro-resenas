e73f616ea8ab8417bc8c1d64c9d87837085c2f78
mauri <maur.gajardo@duocuc.cl>
2025-05-16 22:57:09 -0400

    Microservicio Reseña terminado

---
+++ resena/src/test/java/com/perfulandia/resena/ResenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.resena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
